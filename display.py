564677acdd4fa0d589d816fc6f1c0507948861da
Diego Torres Quintanilla <diegoalbertotorres@gmail.com>
2016-02-02 00:56:59 -0500

    Working for basic case, assumes artwork is stored in music file's directory.

---
+++ display.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+import sys
+import subprocess
+import os.path
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from mpd import MPDClient
+
+global app, label, mpdClient, dim, libPath, defaultImage
+
+def getAlbumName():
+    songPath = libPath + '/' + mpdClient.currentsong()['file']
+    songDir = os.path.dirname(songPath)
+    candidates = ['Folder.jpg', 'Folder.png']
+    for candidate in candidates:
+        path = songDir + '/' + candidate
+        if os.path.isfile(path):
+            return path
+    return None
+
+def initMPDClient(socket):
+    global mpdClient
+    mpdClient = MPDClient()
+    mpdClient.timeout = 10
+    mpdClient.connect(socket, 0)
+
+def quit():
+    sys.exit(app.exec_())
+
+def initWindow():
+    global label
+    w = QWidget()
+    w.setWindowTitle('Artwork Display')
+    w.setAttribute(Qt.WA_TranslucentBackground)
+    QShortcut(QKeySequence('q'), w, quit)
+
+    label = QLabel()
+    layout = QVBoxLayout()
+    layout.setAlignment(Qt.Alignment(Qt.AlignHCenter))
+    w.setLayout(layout)
+    layout.addWidget(label)
+    updateLabel()
+
+    return w
+
+def updateLabel():
+    albumName = getAlbumName()
+    pixmap = QPixmap(albumName)
+    if albumName == None or pixmap.isNull():
+        pixmap = QPixmap(defaultImage)
+    pixmap = pixmap.scaled(dim, dim, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+    label.setPixmap(pixmap)
+
+def main():
+    global label, dim, libPath, app, defaultImage
+
+    mpdSocket = sys.argv[1]
+    libPath = sys.argv[2]
+    dim = int(sys.argv[3])
+    defaultImage = sys.argv[4]
+    if len(sys.argv) >= 6:
+        interval = int(sys.argv[5])
+    else:
+        interval = 1000
+
+    app = QApplication(sys.argv)
+    initMPDClient(mpdSocket)
+    w = initWindow()
+
+    # Refresh artwork every second
+    t = QTimer()
+    t.setInterval(interval)
+    t.timeout.connect(updateLabel)
+    t.start()
+
+    w.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
